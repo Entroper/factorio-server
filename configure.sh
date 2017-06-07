2c9f46e530c30bb0c145ec42d5133a239389f802
Entroper <entroper@vt.edu>
2017-06-07 01:54:25 -0400

    make path optional

--- configure.sh
+++ configure.sh
@@ -1,10 +1,16 @@
-if [ $# -ne 3 ]; then
-	echo "Usage: ./configure.sh <path-to-server> <name-of-server-config> <name-of-map-config>"
+if [[ $# -ne 3 ]] && [[ $# -ne 2 ]]; then
+	echo "Usage: ./configure.sh <name-of-server-config> <name-of-map-config> [path-to-server]"
 	echo "Do not include the config subdirectory or the .json extension."
 	exit 1
 fi
 
-cp settings/server/${2}.json ${1}/config/server-settings.json
-cp settings/map/${3}.json ${1}/config/map-settings.json
-cp settings/map-gen/${3}.json ${1}/config/map-gen-settings.json
+if [ $# -eq 3 ]; then
+	path=$3
+else
+	path="/opt/factorio"
+fi
+
+cp settings/server/${1}.json ${path}/config/server-settings.json
+cp settings/map/${2}.json ${path}/config/map-settings.json
+cp settings/map-gen/${2}.json ${path}/config/map-gen-settings.json
 
--- init.sh
+++ init.sh
@@ -1,13 +1,20 @@
 if [ -z "$1" ]; then
+	path="/opt/factorio"
+else
+	path=$2
+fi
+
+if [ -z "$2" ]; then
 	name="factorio"
 else
 	name=$1
 fi
 
+
 docker run -d -it \
 	-p 34197:34197/udp \
 	-p 27015:27105/tcp \
-	-v /opt/factorio:/factorio \
+	-v $path:/factorio \
 	--name $name \
 	--restart=always \
 	dtandersen/factorio
